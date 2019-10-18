6600b4c64942fa71beb9a2ad26866091b0cf36a4
shimencaiji <56704491+shimencaiji@users.noreply.github.com>
2019-10-18 19:17:43 +0800

    修改range后限制，要＋1

--- get_dividend_data.py
+++ get_dividend_data.py
@@ -2,7 +2,7 @@
 @Description: 从baostock获取股票除权除息信息数据（预披露、预案、正式都已通过）。
 @Author: 石门菜鸡
 @Date: 2019-10-18 15:38:07
-@LastEditTime: 2019-10-18 17:11:33
+@LastEditTime: 2019-10-18 19:16:58
 @LastEditors: Please set LastEditors
 '''
 import baostock as bs
@@ -13,7 +13,7 @@ import pandas as pd
 @param {stock_number:股票代码
         stock_name:股票名称
         start_year:查询起始年度,int类型
-        end_year:查询截至年度
+        end_year:查询截至年度,int类型
         yearType:年份类别，默认为"report":预案公告年份，可选项"operate":除权除息年份。
         } 
 @return: 
@@ -31,7 +31,7 @@ def get_dividend_data(stock_number,stock_name,start_year,end_year,yearType):
           print("登录失败")
 
     rs_list=[]
-    for iter in range(start_year,end_year):
+    for iter in range(start_year,end_year+1):
         rs_dividend=bs.query_dividend_data(code=stock_number,year=iter,yearType=yearType)
         while (rs_dividend.error_code == '0')& rs_dividend.next():
             rs_list.append(rs_dividend.get_row_data())
